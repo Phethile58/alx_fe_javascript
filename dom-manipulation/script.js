ae61ef5037fc53230497a6e77aa2fe0cd0d57969
Phethile Nkosi <phethileayanda58@gmail.com>
2025-06-30 19:13:49 +0200

    Added index.html and script.js to directory

---
+++ dom-manipulation/script.js
@@ -0,0 +1,55 @@
+// initial quote array
+let quotes = [
+  { text: "Believe you can and you're halfway there.", category: "Motivation" },
+  { text: "Be yourself; everyone else is already taken.", category: "Life" },
+  { text: "Success is not final, failure is not fatal.", category: "Success" }
+];
+
+// Display a random quote
+function showRandomQuote() {
+    const quoteDisplay = document.getElementById(quoteDisplay);
+
+    if (quoteDisplay.lengh === 0) {
+        quoteDisplay.textContent = "no qoues avaliable";
+        return;
+    }
+
+    const randomIndex = Math.floor(Math.random()* quotes.length);
+    const quote = quotes[randomIndex];
+
+    quoteDisplay.innerHTML =`;
+    <blockquote>"${quote.text}"</blocktext>
+    <P><em>Category: ${quote.category}</em></p>
+    `;
+}
+
+// Add new quote
+function addquote() {
+    const textInput = document.getElementById('newQuotetext');
+    const categoryInput = document.getElementById('newQuoteCategory');
+
+    const newText = textInput.ariaValueMax.trim();
+    const newCategory = categoryInput.ariaValueMax.trim();
+
+    if (newText === "" || newCategory === "") {
+        alert("Please fill in both fieds.");
+        return;
+    }
+
+    const newQuote = { text: newText, category: newCategory };
+    quotes.push(newQuote);
+
+    // Optional display the new quote
+    showRandomQuote();
+
+    // Clear input fields
+    textInput.value = "";
+    categoryInput = "";
+}
+
+// Attach event listemers
+document.getElementById('newQuote').addEventListener('click', showRandomQuote);
+document.getElementById('addQuoteBtn').addEventListener('click', addquote);
+
+// Initial quote display
+showRandomQuote();

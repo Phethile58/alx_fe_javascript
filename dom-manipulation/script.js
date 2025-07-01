ca070e0045b987e7f9e65a8158090df5cde6db31
Phethile Nkosi <phethileayanda58@gmail.com>
2025-07-01 10:39:58 +0200

    Added script.js to directory

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -7,18 +7,18 @@ let quotes = [
 
 // Display a random quote
 function showRandomQuote() {
-    const quoteDisplay = document.getElementById(quoteDisplay);
+    const quoteDisplay = document.getElementById("quoteDisplay");
 
-    if (quoteDisplay.lengh === 0) {
-        quoteDisplay.textContent = "no qoues avaliable";
+    if (quote.lengh === 0) {
+        quoteDisplay.textContent = "no quotes avaliable";
         return;
     }
 
-    const randomIndex = Math.floor(Math.random()* quotes.length);
+    const randomIndex = Math.floor(Math.random() * quotes.length);
     const quote = quotes[randomIndex];
 
-    quoteDisplay.innerHTML =`;
-    <blockquote>"${quote.text}"</blocktext>
+    quoteDisplay.innerHTML = `
+    <blockquote>"${quote.text}"</blockquote>
     <P><em>Category: ${quote.category}</em></p>
     `;
 }
@@ -28,8 +28,8 @@ function addquote() {
     const textInput = document.getElementById('newQuotetext');
     const categoryInput = document.getElementById('newQuoteCategory');
 
-    const newText = textInput.ariaValueMax.trim();
-    const newCategory = categoryInput.ariaValueMax.trim();
+    const newText = textInput.Value.trim();
+    const newCategory = categoryInput.Value.trim();
 
     if (newText === "" || newCategory === "") {
         alert("Please fill in both fieds.");
@@ -44,7 +44,7 @@ function addquote() {
 
     // Clear input fields
     textInput.value = "";
-    categoryInput = "";
+    categoryInput.value = "";
 }
 
 // Attach event listemers
